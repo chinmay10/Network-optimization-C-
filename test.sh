c80193876c6447bd8beebcc748a22e6a01bcc6b5
Chinmay Ajit Sawkar <chinmay997@gmail.com>
2023-11-05 23:37:34 -0800

    refactoring

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
 
 # About
-echo "=================================================="
-echo "Course Project"
+echo "------------------------------------------------------------------------"
+echo "Network Optimization"
 echo "CSCE 629 Analysis of Algorithms"
-echo "=================================================="
+echo "Author: Chinmay Ajit Sawkar"
+echo "------------------------------------------------------------------------"
 make clean; make all
 echo " "
 
+#output generation
 > output.txt
 ./graph | tee -a output.txt
