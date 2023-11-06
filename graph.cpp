f0930e533f00ea2c049dc75f69d239fb8b982e04
Chinmay Ajit Sawkar <chinmay997@gmail.com>
2023-11-05 23:14:45 -0800

    implemneted graph generation and main testing setup

---
+++ graph.cpp
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include "graph.h"
+#include <iostream>
+
+void Graph::init()
+{
+  for (int i = 0; i < n; ++i) {
+    edges[i] = new Node(i, 0);
+  }
+}
+
+void Graph::addEdge(int s, int t, int wt = RAND_BW)
+{
+  Node *src = new Node(s, wt);
+  Node *dest = new Node(t, wt);
+
+  Node *x = edges[s];
+  while (x->next != NULL)
+    x = x->next;
+  x->next = dest;
+
+  Node *y = edges[t];
+  while (y->next != NULL)
+    y = y->next;
+  y->next = src;
+}
+
+bool Graph::isEdge(int s, int t)
+{
+  Node *x = edges[s];
+  while (x != NULL) {
+    if (x->vertex == t)
+      return true;
+    x = x->next;
+  }
+  return false;
+}
+
+void Graph::generateGraph1()
+{
+  int deg[5000] = {0};
+  init();
+
+  for (int i = 0; i < n; i++)
+  {
+    int j = (i + 1) % n;
+    addEdge(i, j);
+    deg[i]++;
+    deg[j]++;
+  };
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (deg[i] == 6) break;
+      int x = rand() % n;
+      if (!isEdge(i, x))
+      {
+        if(deg[i] < degree && deg[x] < degree) {
+          addEdge(i, x);
+          deg[i] = deg[i] + 1;
+          deg[x] = deg[x] + 1;
+        }
+      }
+    }
+  }
+}
+
+void Graph::generateGraph2()
+{
+  init();
+  for (int i = 0; i < n; i++) {
+    int j = (i + 1) % n;
+    addEdge(i, j);
+  };
+
+  for (int i = 0; i < n; i++) {
+    int j = 0;
+    int num = ((percent*n/100)*0.5 - 50) + rand() % 100;
+    while(j < num) {
+      int x = rand() % n;
+      if (!isEdge(i, x) && i != x) {
+        addEdge(i, x);
+        j++;
+      }
+      else j--;
+    }
+  }
+}
---
+++ test.sh
@@ -0,0 +1,11 @@
+
+# About
+echo "=================================================="
+echo "Course Project"
+echo "CSCE 629 Analysis of Algorithms"
+echo "=================================================="
+make clean; make all
+echo " "
+
+> output.txt
+./graph | tee -a output.txt
